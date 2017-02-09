492925fbc8e62cdd294ff11e272dc1c079b5e29d
Davis Herbig <davis.herbig@gmail.com>
2017-02-09 12:19:06 -0800

    Fixed Table namespace.

--- Table.cs
+++ Table.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace BlackJack{
+namespace blackJack{
     // This section holds all the players in the game, and allows turn order to be tracked.
     public class Table
     {
