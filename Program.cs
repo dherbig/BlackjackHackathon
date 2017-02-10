974fa0dc2cba9062d9e65db25928ac00b59515d5
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-09 16:18:59 -0800

    Post Bug Fix Session

--- Program.cs
+++ Program.cs
@@ -2,60 +2,60 @@
 
 namespace blackJack {
     public class Program {
-        public static void Main (string [] args) {
+        public static void Main (string[] args) {
             Table MyTable = new Table (1);
             Turn MyTurn = new Turn ();
             bool GameOn = true;
-            int rounds = 0;
+            int rounds = 1;
             while (GameOn == true) {
                 // Deal each player up card
                 foreach (Player player in MyTable.PlayerList) {
-                    player.Draw (MyTable.deckName);
-                    player.hand [0].faceDown = true;
+                    player.SecretDraw (MyTable.deckName);
                 }
                 // deal each player a face down card
                 foreach (Player player in MyTable.PlayerList) {
-                    player.Draw (MyTable.deckName);
-                    player.hand [0].faceDown = false;
+                    player.SecretDraw (MyTable.deckName);
+                    if (player==MyTable.PlayerList[0]){
+                        player.hand[0].faceDown = true;
+                }
                 }
                 // each player takes a turn
                 foreach (Player player in MyTable.PlayerList) {
                     // skip dealer.
-                    if (player == MyTable.PlayerList [0]) {
+                    if (player == MyTable.PlayerList[0]) {
                         continue;
                     }
-                    // display all cards based upon their status
-                    MyTurn.showTable (MyTable);
-                    // flip player's cards
-                    MyTurn.showHand (player);
                     // start hand loop
                     string choice = "";
-                    while (choice != "stand")
+                    while (choice != "STAND") {
+                        // display all cards based upon their status
+                        MyTurn.showTable (MyTable);
                         // show total
                         System.Console.WriteLine ("Your total is: " + MyTurn.checkTotal (player).ToString ());
-                    // hit or stand
-                    choice = MyTurn.hitOrStand (player);
-                    if (choice == "Hit") {
-                        player.Draw (MyTable.deckName);
+                        // hit or stand
+                        choice = MyTurn.hitOrStand (player);
+                        if (choice == "HIT") {
+                            player.Draw (MyTable.deckName);
+                        }
+                        // calculate new total
+                        int playerVal = MyTurn.checkTotal (player);
+                        if (playerVal < 0) {
+                            break;
+                        }
+                        // repeat till stand or busted.
                     }
-                    // calculate new total
-                    int playerVal = MyTurn.checkTotal (player);
-                    if (playerVal < 0) {
-                        break;
-                    }
-                    // repeat till stand or busted.
                 }
                 // dealer takes turn
-                MyTable.PlayerList [0].dealerRound (MyTable, MyTurn);
+                MyTable.PlayerList[0].dealerRound (MyTable, MyTurn);
                 // check winners
                 string results = MyTurn.checkWinner (MyTable);
                 System.Console.WriteLine (results);
-                
+
                 foreach (Player player in MyTable.PlayerList) {
-                    if (player == MyTable.PlayerList [0]) {
+                    if (player == MyTable.PlayerList[0]) {
                         continue;
                     }
-                    System.Console.WriteLine (player.name + "'s win percent is " + player.wins / rounds);
+                    System.Console.WriteLine (player.name + "'s win percent is " + ((int) ((player.wins / rounds) * 100)).ToString () + "%");
                 }
                 // quit or continue
                 //    query the player quit or continue
@@ -63,6 +63,12 @@ namespace blackJack {
                 string playerQuit = System.Console.ReadLine ();
                 if (playerQuit == "Quit") {
                     GameOn = false;
+                }
+                MyTurn.resetHand (MyTable);
+                rounds = rounds + 1;
+                if (rounds % 3 == 0) {
+                    System.Console.WriteLine ("Shuffling");
+                    MyTable.deckName.Shuffle ();
                 }
             }
 
